6703712efd2c9c2ee5896250e2138e4d1b189749
redjax <none@none.com>
2023-07-12 01:32:36 -0400

    Update README, example env, reload script:
    Add a simple README
    
    Update docker-compose's .env.example
    
    Update reload_nginx.sh, use docker compose exec (vs docker exec {container's name, changes each project})

--- project/reload_nginx.sh
+++ project/reload_nginx.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-CONTAINER_NAME="medcab_proxy"
-
-echo "Reloading NGINX container [$CONTAINER_NAME]"
-
-docker exec $CONTAINER_NAME nginx -s reload
+docker compose exec proxy nginx -s reload
 
 exit $?
