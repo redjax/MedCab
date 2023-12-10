d490c42d2db7c4b8b52fac2bbd194b634272e9bc
redjax <none@none.com>
2023-12-10 15:22:41 -0500

    Refactor manage_db app. Move functions into operations.py, fix imports

--- project/apps/api/src/manage_db/__init__.py
+++ project/apps/api/src/manage_db/__init__.py
@@ -1,1 +1,5 @@
-from .main import insert_samples, API, EXAMPLE_SCHEMAS_DIR, EXAMPLE_PRODUCTS_DIR, EXAMPLE_SIMPLIFIED_PRODUCTS_DIR
+import sys
+sys.path.append(".")
+
+from .operations import insert_samples, remove_all_items
+from .main import API, EXAMPLE_SCHEMAS_DIR, EXAMPLE_PRODUCTS_DIR, EXAMPLE_SIMPLIFIED_PRODUCTS_DIR
--- project/apps/api/src/manage_db/__main__.py
+++ project/apps/api/src/manage_db/__main__.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append(".")
 
-from manage_db import insert_samples, API, EXAMPLE_SCHEMAS_DIR, EXAMPLE_PRODUCTS_DIR, EXAMPLE_SIMPLIFIED_PRODUCTS_DIR
+from manage_db import insert_samples, remove_all_items, API, EXAMPLE_SCHEMAS_DIR, EXAMPLE_PRODUCTS_DIR, EXAMPLE_SIMPLIFIED_PRODUCTS_DIR
 from core.config import app_settings
 
 from red_utils.ext.loguru_utils import init_logger, LoguruSinkStdOut
@@ -13,3 +13,5 @@ if __name__ == "__main__":
 
     log.info(f"[env:{app_settings.env}|container:{app_settings.container_env}] App Start")
     insert_samples(api=API, products_json_dir=EXAMPLE_SIMPLIFIED_PRODUCTS_DIR)
+    
+    remove_all_items(api=API)
