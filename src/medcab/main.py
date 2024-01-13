0c71fe76baa4d0317895e5fcae051e7dd3e76c5f
redjax <none@none.com>
2024-01-13 16:24:27 -0500

    Add example purchases to main.py.
    Move demo functionality to demo() function

--- src/medcab/main.py
+++ src/medcab/main.py
@@ -3,18 +3,15 @@ from __future__ import annotations
 from core.dependencies import APP_SETTINGS, ENSURE_DIRS
 from domain.product import Product
 from domain.purchase import Purchase
+from domain.dispensary import Dispensary
+
 from examples.products import load_example_products_simplified
 
 from red_utils.std.path_utils import ensure_dirs_exist
 from loguru import logger as log
 from red_utils.ext.loguru_utils import LoguruSinkStdOut, init_logger
 
-if __name__ == "__main__":
-    init_logger([LoguruSinkStdOut(level=APP_SETTINGS.log_level).as_dict()])
-    log.info(f"Settings: {APP_SETTINGS}")
-
-    ensure_dirs_exist(ENSURE_DIRS)
-    
+def demo():
     ex_products = load_example_products_simplified()
     log.debug(f"Example Products: {ex_products}")
     
@@ -27,4 +24,14 @@ if __name__ == "__main__":
     for p in products:
         log.debug(f"Product ({type(p)}): {p}")
         purchase: Purchase = Purchase(date="2024-01-13", product=p)
+        purchase.dispensary = Dispensary(name="Rise Cannabis", city="Cleveland", state="OH")
+        purchase.price = 36.00
         log.debug(f"Purchase: {purchase}")
+
+if __name__ == "__main__":
+    init_logger([LoguruSinkStdOut(level=APP_SETTINGS.log_level).as_dict()])
+    log.info(f"Settings: {APP_SETTINGS}")
+
+    ensure_dirs_exist(ENSURE_DIRS)
+    
+    demo()    
