1f10170c7fe9cc89cf87396dc5c99eeb0c18729d
redjax <none@none.com>
2024-01-13 15:11:40 -0500

    Add examples module, load examples from data dir

---
+++ src/medcab/examples/products/__init__.py
@@ -0,0 +1,1 @@
+from .operations import load_example_products, load_example_products_simplified
--- src/medcab/main.py
+++ src/medcab/main.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
 from core.dependencies import APP_SETTINGS, ENSURE_DIRS
-from red_utils.std.path_utils import ensure_dirs_exist
+from examples.products import load_example_products_simplified
 
+from red_utils.std.path_utils import ensure_dirs_exist
 from loguru import logger as log
 from red_utils.ext.loguru_utils import LoguruSinkStdOut, init_logger
 
@@ -11,3 +12,6 @@ if __name__ == "__main__":
     log.info(f"Settings: {APP_SETTINGS}")
 
     ensure_dirs_exist(ENSURE_DIRS)
+    
+    ex_products = load_example_products_simplified()
+    log.debug(f"Example Products: {ex_products}")
